cb97ef0aaafd94fd2d82767624ae16a84e72ed75
Todd Davis <tejastcd@gmail.com>
2019-07-26 11:31:19 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+    DATABASE_URL: process.env.DATABASE_URL || 'dummy-db-connection-url',
+}
